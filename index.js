75757355d82edac732396777ee157dff9ba06ed4
Jake Romer <jkrmr@jakeromer.com>
2017-09-07 19:03:46 -0400

    Add processing of command-line args for source/target dirs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fs from 'fs-extra'
+import parseArgs from 'minimist'
 
 import FileInfo from './lib/file_info'
 import MoveAnnotated from './lib/move_annotated'
@@ -6,31 +7,41 @@ import Results from './lib/results'
 import completionMessage from './lib/completion_message'
 import timestamp from './lib/timestamp'
 
-const ORIGINAL_PATH = './files/original'
-const NEW_PATH = './files/moved'
+// Parse command-line arguments
+const { source, target, s, t } = parseArgs(process.argv.slice(2))
+const ORIGINAL_PATH = source || s || './files/original'
+const NEW_PATH = target || t || './files/moved'
+
 const VALID_FILE_EXTENSIONS = /\.txt$|\.json$/
 const results = new Results()
 
-fs.readdir(ORIGINAL_PATH)
-  .then(files => {
-    return files
-      .filter(name => VALID_FILE_EXTENSIONS.test(name))
-      .map(originalName => {
-        const fileInfo = new FileInfo({
-          sourceDir: ORIGINAL_PATH,
-          filename: originalName,
-          targetDir: NEW_PATH,
-          timestamp: timestamp()
-        })
-
-        return MoveAnnotated.fileWithInfo(fileInfo)
-          .then(() => results.addSuccess(originalName))
-          .catch(error => {
-            console.log(error.message)
-            results.addFailure(originalName)
-          })
-      })
+// Process a single file:
+// Parse file info and save, annotated, to target directory
+const processFile = (filename) => {
+  const fileInfo = new FileInfo({
+    sourceDir: ORIGINAL_PATH,
+    filename: filename,
+    targetDir: NEW_PATH,
+    timestamp: timestamp()
   })
+
+  return MoveAnnotated.fileWithInfo(fileInfo)
+    .then(() => results.addSuccess(filename))
+    .catch(error => {
+      console.log(error.message)
+      results.addFailure(filename)
+    })
+}
+
+// read contents of source directory, process valid files,
+// handle errors and generate completion message
+fs.readdir(ORIGINAL_PATH)
+  .then(files => files
+    .filter(name => VALID_FILE_EXTENSIONS.test(name))
+    .map(originalName => processFile(originalName)))
   .then(renameOps => Promise.all(renameOps))
   .then(() => console.log(completionMessage(results)))
-  .catch(error => console.log(error))
+  .catch(error => {
+    console.log(error)
+    console.log('renamed 0 files: source directory not found')
+  })
