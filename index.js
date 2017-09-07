e3ba431fc5500a986d0747a59047ecbb89518354
Jake Romer <jkrmr@jakeromer.com>
2017-09-07 18:41:17 -0400

    Only process valid files: txt, json
    Fixes exceptions encountered because of .DS_STORE files.

--- index.js
+++ index.js
@@ -8,25 +8,28 @@ import timestamp from './lib/timestamp'
 
 const ORIGINAL_PATH = './files/original'
 const NEW_PATH = './files/moved'
+const VALID_FILE_EXTENSIONS = /\.txt$|\.json$/
 const results = new Results()
 
 fs.readdir(ORIGINAL_PATH)
   .then(files => {
-    return files.map(originalName => {
-      const fileInfo = new FileInfo({
-        sourceDir: ORIGINAL_PATH,
-        filename: originalName,
-        targetDir: NEW_PATH,
-        timestamp: timestamp()
-      })
-
-      return MoveAnnotated.fileWithInfo(fileInfo)
-        .then(() => results.addSuccess(originalName))
-        .catch(error => {
-          console.log(error.message)
-          results.addFailure(originalName)
+    return files
+      .filter(name => VALID_FILE_EXTENSIONS.test(name))
+      .map(originalName => {
+        const fileInfo = new FileInfo({
+          sourceDir: ORIGINAL_PATH,
+          filename: originalName,
+          targetDir: NEW_PATH,
+          timestamp: timestamp()
         })
-    })
+
+        return MoveAnnotated.fileWithInfo(fileInfo)
+          .then(() => results.addSuccess(originalName))
+          .catch(error => {
+            console.log(error.message)
+            results.addFailure(originalName)
+          })
+      })
   })
   .then(renameOps => Promise.all(renameOps))
   .then(() => console.log(completionMessage(results)))
